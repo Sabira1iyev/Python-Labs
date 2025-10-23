6b7379ab0286aedf1f70754b96da4bb7ee68504f
Sabira1iyev <s.aliyev2005@gmail.com>
2025-10-23 10:45:12 -0400

    changing second time

--- main4.py
+++ main4.py
@@ -49,10 +49,90 @@ def user_func(a, b = 2):
 print(user_func(1))
 print(user_func(1,9))
 
-'''
+---------
 
 def user_func(*, a,b, c=10):
     return a+ b+c
-c = user_func(1,2,1)
+#c = user_func(1,2,1)
 #c = user_func(a = 1, b=5)
 print(c)
+
+------
+
+def Sum(*t):
+    summ = 0
+    for i in t:
+        summ += i
+    return summ
+print(f"Zero parameter: Sum() = {Sum()}")
+print(f"Two parameter: sum(10,20) = {Sum(10,20)}")
+print(f"Threee parameter: sum(10,20,30) = {Sum(10,20,30)}")
+
+--------
+
+def Sum(**t):
+    summ = 0
+    for i in t.keys():
+        summ += t[i]
+    return summ
+print(Sum())
+print(Sum(x = 10, y = 20))
+print(Sum(a=10,b=20,c=30))
+
+---------
+
+def Sum(**t):
+    summ = 0;
+    for i in t.values():
+        summ += i
+    return summ
+print(Sum())
+print(Sum(x = 10, y= 20))
+print(Sum(a = 10, b= 20, c= 30))
+
+--------
+
+def Sum(**t):
+    summ = 0
+    for key,value in t.items():
+        summ += value
+    return summ
+print(Sum())
+print(Sum(x = 10, y=20))
+print(Sum(a=10,b=20,c=30))
+
+
+----------
+
+def Sum(a,b):
+    return a + b
+
+def print_sum(f,a,b):
+    print(f"{a} + {b} = {f(a,b)}")
+
+print_sum(Sum,1,2)
+
+------------
+
+def print_sum(a,b):
+    def Sum():
+        return a+ b
+    print(f"{a} + {b} = {Sum()}")
+    return Sum
+print(print_sum(1,2))
+
+
+'''
+
+def bank_account(balance):
+    def deposit(amount):
+        nonlocal balance
+        balance += amount
+        return balance
+    def withdraw(amount):
+        nonlocal balance
+        balance -= amount
+        return balance
+    return deposit, withdraw
+
+deposit, withdraw = bank_account(100)
