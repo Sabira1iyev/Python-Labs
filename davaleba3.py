13a645c5af816c3c0c8555445c63dc7038cc89bf
Sabir Aliyev <s.aliyev2005@gmail.com>
2025-12-09 01:21:24 +0400

    davaleba-3

--- davaleba3.py
+++ davaleba3.py
@@ -1,1 +1,8 @@
-print("hello world");
+import os
+import numpy as np
+from typing import List, Tuple, Optional
+
+class KramerProcess:
+    def __init__(self,filename:str):
+        self.filename = filename
+        self.lines: Optional[np.ndarray] = none
