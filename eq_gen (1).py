06c7b6f90e658a31a079275fedadca62ca553b27
Sabir Aliyev <s.aliyev2005@gmail.com>
2025-12-09 00:15:05 +0400

    davaleba-3

---
+++ eq_gen (1).py
@@ -0,0 +1,24 @@
+from random import randint as ri
+
+def matr_gen(v_low=20, v_hight=30, n_low=20000, n_hight=22000):
+    sz = ri(n_low, n_hight)
+    def gen_row(vnum, count=sz):
+        arr = []
+
+        for _ in range(sz):
+            num = str(ri(1, vnum + 1))
+            a = f"x{num}" if ri(0, 1) else f"-x{num}"
+            if num == str(vnum + 1):
+                a = "-b" if a[0] == '-' else "b"
+            arr.append(a)
+        return arr
+    row_num = ri(v_low, v_hight)
+    return '\n'.join(' '.join(gen_row(row_num)) for _ in range(row_num))
+
+for i in range(ri(20,30)):
+    st = matr_gen()
+    with open(f'file_{i}.in', 'w') as fd:
+        fd.write(st)
+
+
+
